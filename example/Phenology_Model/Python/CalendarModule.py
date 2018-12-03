5fcdf73793515460e6fda65efdb4fa1fc9427f8d
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2018-12-03 15:25:04 +0100

    some changes

--- example/Phenology_Model/Python/CalendarModule.py
+++
@@ -1,8 +0,0 @@
-import Calendar
-import Phase
-def CalculateCalendar(phase, currentdate, cumulTT,calendar):
-    if (Calendar.IsMomentRegistred(Phase.PreviousMoment(phase), calendar) == 0):
-        calendar = Calendar.CalendarSet(Phase.PreviousMoment(phase), currentdate, cumulTT, calendar)
-    return calendar
-
-
--- example/Phenology_Model/Python/IsMomentRegistredModule.py
+++
@@ -1,4 +0,0 @@
-import Calendar
-def IsMomentRegistred(calendar):
-    isMomentRegistredZC_39 = Calendar.IsMomentRegistred("ZC_39_FlagLeafLiguleJustVisible", calendar);
-    return isMomentRegistredZC_39
